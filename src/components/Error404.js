94877dd51c04587b288b503be6a33368208779a1
lucasdmadrigal <lucasmadrigalb@gmail.com>
2021-01-21 18:32:39 -0300

    primer commit - paquetes webpack y babel instalados

---
+++ src/components/Error404.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Error404 = () => <h2>Error 404</h2>
+
+export default Error404
---
+++ src/components/Helloworld.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Helloworld = () => <h1>Hello world</h1>;
+export default Helloworld;
